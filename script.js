f94a4c8d71820a8995107392bf5e4dd9f29f951a
Draquix <Draquix@yahoo.com>
2020-12-17 13:07:44 -0600

    added function that checks for valid input of a color and returns false if not a color, and a while loop that gives three chances to type a valid color.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//colorSelect needs to be global to work in two spots
+var colorSelect;
 // Fun discovery, multiple items with the same class are stored as an array
 //when you use 'document.getElementsByClassName('class') = variable;' method
 
@@ -12,18 +14,45 @@ function nameToClass(name){
         indexClass[i].innerHTML = name;
     }
 }
+
+/* To further meet requirements of the assignment, this function
+checks to see if the user input was valid. It checks to see if
+the backgroundColor has a value, and if none it returns false. */
+function colorValidate(){
+    console.log("colorSelect is "+colorSelect);
+    var bGC = document.body.style.backgroundColor;
+    console.log("color is " + bGC);
+    if (bGC === undefined || bGC === "" ){
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
 //This is activated by href tag in the html. It asks three questions by prompt
 //NOTE FOR GRADING -- There is an if statement, the page will use an alert()
 // to call you out for Red Green or Blue.
 
-function pageColorPrompt() {
+//Lab07 edit:!!: There is now a while loop that gives 3 chances after the first to enter a proper color.
+//If something like 'apple' or 'truck' is entered it prompts again for an actual color. The mechanism is that
+//there is now a while loop that counts attempts with a variable and prompts again and again as long as
+//the colorValidate function is returning false or the counter variable is under 4. This meets the stretch goal.
+function pagePrompt() {
+    var counter = 0;
     var name = prompt("What is your name?");
     var quest = prompt("What is your quest?");
     var colorSelect = prompt("What is your favorite color?");
+    console.log("colorSelect is entered as "+colorSelect);
     if (colorSelect === "Blue" || colorSelect === "Red" ||colorSelect ===  "Green") {
         alert("That's a terrible color...");
     }
     document.body.style.backgroundColor = colorSelect;
+    while (colorValidate() === false && counter < 3) {
+        counter++;
+        colorSelect = prompt("Please enter a valid color. (try "+counter+" of 3)");
+        document.body.style.backgroundColor = colorSelect;
+    }
     document.getElementById('quest').innerHTML = quest;
     nameToClass(name);
 }
